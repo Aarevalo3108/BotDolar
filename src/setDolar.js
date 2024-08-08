29909fd2ab9172aee104ee695012c8c5d52013f7
Angel Arevalo <angelarevalo480@gmail.com>
2024-08-08 19:41:56 -0400

    [change]: url base not longer supported, new one added

--- src/setDolar.js
+++ src/setDolar.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 import {colors} from '@gamastudio/colorslog';
 
-const BCV = "https://pydolarvenezuela-api.vercel.app/api/v1/dollar/unit/bcv"
-const ENP = "https://pydolarvenezuela-api.vercel.app/api/v1/dollar/unit/enparalelovzla"
+const url = "http://pydolarve.org/api/v1/dollar";
 const setDolar = async (array) => {
 	try
 	{
-		const bcv = await axios.get(BCV)
-		const enp = await axios.get(ENP)
-		array[0] = bcv.data.price.toFixed(2);
-		array[1] = enp.data.price.toFixed(2);
-		colors.success(`[DOLAR_SETTED]: BCV: ${bcv.data.price.toFixed(2)}, ENP: ${enp.data.price.toFixed(2)}`)
-    return array
+		const dolar = await axios.get(url);
+		array[0] = dolar.monitors.bcv.price.toFixed(2);
+		array[1] = dolar.monitors.enparalelovzla.price.toFixed(2);
+		colors.success(`[DOLAR_SETTED]: BCV: ${bcv.data.price.toFixed(2)}, ENP: ${enp.data.price.toFixed(2)}`);
+    return array;
 	} catch (error) {
 		colors.error(`[DOLAR_ERROR]: ${error}`)
-    return array
+    return array;
 	}
 }
 
